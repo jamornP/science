7a3acfaddaa3b304745382065fd20c1367e26800
Jamorn Pengsuay <jamorn07@gmail.com>
2022-06-07 17:11:09 +0700

    upload

--- sciday/components/navbar.php
+++ sciday/components/navbar.php
@@ -96,7 +96,6 @@
                                         ";
                                     }
                                 ?>
-                                <!-- <a class="dropdown-item" href="/science/sciday/project/member.php">แก้ไขข้อมูลส่วนตัว</a> -->
                                 <hr class="dropdown-divider">
                                 <a class="dropdown-item" href="/science/sciday/auth/logout.php">ออกจากระบบ</a>
                             </div>
@@ -108,10 +107,7 @@
                     <a class="nav-link text-white" href="/science/sciday/auth/login.php"><i class='bx bx-user-circle'></i> เข้าสู่ระบบ</a>
                 <?php }?>
             </div>
-            <!-- <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
+            
         </div>
     </div>
 </nav>
