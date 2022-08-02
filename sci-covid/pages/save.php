cb697329426e34e8a0f514d667812ffb00b4a1e8
Jamorn Pengsuay <jamorn07@gmail.com>
2022-08-02 17:58:08 +0700

    covid

---
+++ sci-covid/pages/save.php
@@ -0,0 +1,11 @@
+<?php require $_SERVER['DOCUMENT_ROOT']."/science/vendor/autoload.php";?>
+<?php 
+
+    echo "<pre>"; 
+    print_r($_REQUEST);
+    echo "</pre>";
+//     echo "<pre>"; 
+//     print_r($_FILES['img_path']);
+//     echo "</pre>";
+
+?>
