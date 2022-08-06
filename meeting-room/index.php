1dca97df78260ef060254fac1e434dae64996cd6
Jamorn Pengsuay <jamorn07@gmail.com>
2022-08-06 13:52:19 +0700

    meeting

---
+++ meeting-room/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ระบบจองห้องประชุม</title>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/science/meeting-room/components/link.php";?>
+</head>
+
+<body>
+<?php //header('Location: /science/sci-covid/pages'); ?>
+</body>
+
+</html>
