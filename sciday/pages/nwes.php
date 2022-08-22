543c543256992a40436fcb38a3b79946359727e8
Jamorn Pengsuay <jamorn07@gmail.com>
2022-08-22 14:38:35 +0700

    upweb

---
+++ sciday/pages/nwes.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sci-Day2022</title>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/science/sciday/components/link.php";?>
+</head>
+
+<body class="font-prompt">
+    <?php require $_SERVER['DOCUMENT_ROOT']."/science/sciday/components/navbar.php";?>
+    <div class="container mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <img src='/science/sciday/images/001.png' class='img-thumbnail' alt=''>
+                </div>
+            
+                <div class="col-lg-6 col-md-6">
+                <img src='/science/sciday/images/002.png' class='img-thumbnail mt-2' alt=''>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                <img src='/science/sciday/images/003.png' class='img-thumbnail mt-2' alt=''>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                <img src='/science/sciday/images/004.png' class='img-thumbnail mt-2' alt=''>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                <img src='/science/sciday/images/005.png' class='img-thumbnail mt-2' alt=''>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
