a522fc35425b4132805754630e38d405256b89cf
Jamorn Pengsuay <jamorn07@gmail.com>
2022-09-29 23:32:23 +0700

    upweb

--- sciday/certificate/addcertificate.php
+++ sciday/certificate/addcertificate.php
@@ -26,7 +26,7 @@
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{ "studentName": "","certificateName": "นิทรรศการวันวิทยาศาสตร์ Science for every Generation","createDate": "2022-09-16T00:00:00+07:00", "sortBy": "batchId,", "sortDirectionBy": "DESC", "limit": { "pageSize": 1000, "pageNumber": 1 }}',
+        CURLOPT_POSTFIELDS =>'{ "studentName": "","certificateName": "นิทรรศการวันวิทยาศาสตร์ Science for every Generation","createDate": "2022-09-09T00:00:00+07:00", "sortBy": "batchId,", "sortDirectionBy": "DESC", "limit": { "pageSize": 1000, "pageNumber": 1 }}',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
             'Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlNDSURBWSIsImV4cCI6NDcwNzk0NjQ2NSwianRpIjoiZjJmZTk5NGQtYjZkMy00MGQ0LWI4OTItZDc3NDA5MWI0OTE1IiwiaWF0IjoxNjMyMTA2NDY1LCJpc3MiOiJWeER4bk1ycWhPbzZUbmRmZ3NEdkRYdjVWaklzbWxVNSIsIm5iZiI6MTYzMjEwNjQ2NX0.eYSBC-bLhaf9_cnnpXUBtI26MdpmW9-Qf5ZO3axWnpE',
