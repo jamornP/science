d33c5bb65ac9c6393f2e4f7cc6d18156d129f6e6
Jamorn Pengsuay <jamorn07@gmail.com>
2022-09-30 17:01:41 +0700

    แจ้งซ่อม

---
+++ repair/components/link.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="/science/lib/lib/jquery.fancybox.css" media="screen">
+<link rel="stylesheet" href="/science/lib/fullcalendar/fullcalendar.css">
+<link rel="stylesheet" href="/science/lib/fullcalendar/fullcalendar.print.css" media="print">
+<link rel="stylesheet" href="/science/theme/css/bootstrap-theme.css">
+<link rel="stylesheet" href="/science/theme/css/style_car.css">
+<link rel="stylesheet" href="/science/lib/dropzone/min/dropzone.min.css">
+<link rel="stylesheet" href="/science/theme/css/datepicker.css">
+
+<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+
+<script src='/science/lib/lib/jquery/dist/jquery.min.js'></script>
+
+<script src='/science/lib/lib/moment.min.js'></script>
+<script src='/science/lib/fullcalendar/fullcalendar.min.js'></script>
+<script src='/science/lib/lib/lang/th.js'></script>
+<script src='/science/lib/lib/jquery.fancybox.pack.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js'></script>
+<script src="/science/lib/dropzone/min/dropzone.min.js"></script>
+<script src='/science/theme/js/bootstrap-datepicker.js'></script>
+<script src='/science/theme/js/bootstrap-datepicker-thai.js'></script>
+<script src='/science/theme/js/locales/bootstrap-datepicker.th.js'></script>
