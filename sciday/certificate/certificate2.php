3d57d2a958798f73248571805c4116ea7cf4eab6
Jamorn Pengsuay <jamorn07@gmail.com>
2022-10-03 15:48:37 +0700

    upweb

--- sciday/certificate/certificate2.php
+++ sciday/certificate/certificate2.php
@@ -26,7 +26,7 @@
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{ "studentName": "","certificateName": "นิทรรศการวันวิทยาศาสตร์ Science for every Generation","createDate": "2022-09-09T00:00:00+07:00", "sortBy": "batchId,", "sortDirectionBy": "DESC", "limit": { "pageSize": 1000, "pageNumber": 1 }}',
+        CURLOPT_POSTFIELDS =>'{ "studentName": "","certificateName": "นิทรรศการวันวิทยาศาสตร์ Science for every Generation","createDate": "2022-09-11T00:00:00+07:00", "sortBy": "batchId,", "sortDirectionBy": "DESC", "limit": { "pageSize": 1000, "pageNumber": 1 }}',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
             'Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlNDSURBWSIsImV4cCI6NDcwNzk0NjQ2NSwianRpIjoiZjJmZTk5NGQtYjZkMy00MGQ0LWI4OTItZDc3NDA5MWI0OTE1IiwiaWF0IjoxNjMyMTA2NDY1LCJpc3MiOiJWeER4bk1ycWhPbzZUbmRmZ3NEdkRYdjVWaklzbWxVNSIsIm5iZiI6MTYzMjEwNjQ2NX0.eYSBC-bLhaf9_cnnpXUBtI26MdpmW9-Qf5ZO3axWnpE',
@@ -40,7 +40,7 @@
     ?>
     <div class="card">
         <div class="card-header">
-            <a href="/science/sciday/certificate/certificate2.php" class="btn btn-success text-white"> Next >></a>
+            <a href="/science/sciday/certificate/certificate3.php" class="btn btn-success text-white"> Next >></a>
         </div>
         <div class="card-body">
             <table class="table">
@@ -132,9 +132,9 @@
                 </tbody>
             </table>
         </div>
-    </div>
-    <div class="mt-4">
-        <a href="/science/sciday/certificate/certificate2.php" class="btn btn-success"> Next >></a>
+        <div class="mt-4">
+            <a href="/science/sciday/certificate/certificate3.php" class="btn btn-success"> Next >></a>
+        </div>
     </div>
     
 </body>
