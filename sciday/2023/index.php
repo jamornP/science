7a2a2b494141c996af12b951e38f32f79d2334a7
jamornP <100953989+jamornP@users.noreply.github.com>
2023-06-07 10:19:28 +0700

    up facebook

--- sciday/2023/index.php
+++ sciday/2023/index.php
@@ -157,6 +157,7 @@ session_start();
 </head>
 
 <body class="font-sriracha">
+    
     <?php require $_SERVER['DOCUMENT_ROOT'] . "/science/sciday/components/navbar2023.php"; ?>
     <?php $indexs = $adminObj->getIndex(); ?>
     <section class="min-vh-100">
@@ -361,7 +362,36 @@ session_start();
         // การเรียกใช้
         var iCountDown = setInterval("countDown()", 1000);
     </script>
+<!-- Messenger ปลั๊กอินแชท Code -->
+<div id="fb-root"></div>
 
+<!-- Your ปลั๊กอินแชท code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "145367872869812");
+  chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v17.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
 </body>
 
 </html>
