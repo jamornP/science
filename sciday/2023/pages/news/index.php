d28dd68d66ea32c9d611477c267365e34d9137cb
jamornP <100953989+jamornP@users.noreply.github.com>
2023-07-12 14:48:35 +0700

    ีup

--- sciday/2023/pages/news/index.php
+++ sciday/2023/pages/news/index.php
@@ -21,38 +21,40 @@
             <h5 class="card-header bg-187 text-white">ข่าวประชาสัมพันธ์</h5>
             <div class="card-body">
                 <div class="row ">
-                            
+
                     <div class="col-lg-12 col-md-12 ">
                         <div class="div_news">
                             <?php
-                               $news = $adminObj->getNewsAll("data");
-                               $i=0;
-                               foreach($news as $n){
-                                   $i++;
-                                   
-                                       echo "
+                            $news = $adminObj->getNewsAll("data");
+                            $i = 0;
+                            foreach ($news as $n) {
+                                $text_title = nl2br($n['n_title']);
+                                $text_detail = nl2br($n['n_detail']);
+                                $i++;
+
+                                echo "
                                            <div class='card card_news mt-1'>
                                                <div class='card-header  bg-29'>
-                                                   {$n['n_title']} <i class='fs-10'>({$n['n_date']})</i>
+                                                   {$text_title} <i class='fs-10'>({$n['n_date']})</i>
                                                </div>
                                                <div class='card-body fs-14'>
-                                                   <p class='mb-0'>{$n['n_detail']}</p>
+                                                   <p class='mb-0'>{$text_detail}</p>
                                        ";
-                                       $downlons = $adminObj->getDownloadById("data",$n['d_id']);
-                                       $j=0;
-                                       foreach($downlons as $d){
-                                           $j++;
-                                           echo "
+                                $downlons = $adminObj->getDownloadById("data", $n['d_id']);
+                                $j = 0;
+                                foreach ($downlons as $d) {
+                                    $j++;
+                                    echo "
                                                <a href='{$d['d_link']}' class='text-primary me-mr-2' style='text-decoration: none;' target='_blank'><i class='bx bx-file' ></i> {$j}. {$d['d_name']}</a>
                                            ";
-                                       }
-                                       if($n['n_link']=="-"){
-                                           echo "
+                                }
+                                if ($n['n_link'] == "-") {
+                                    echo "
                                                </div>
                                            </div>
                                            ";
-                                       }else{
-                                       echo"            
+                                } else {
+                                    echo "            
                                                    
                                                    <br>
                                                    <a href='{$n['n_link']}' class='btn btn-sm btn-warning fs-12' target='_blank'>รายละเอียด...</a>    
@@ -60,10 +62,9 @@
                                                </div>
                                            </div>
                                        ";
-                                       }
-                                  
-                               }
-                               
+                                }
+                            }
+
                             ?>
                         </div>
                     </div>
